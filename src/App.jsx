dc37e2ca75f08af1850f36f50c632092f4206131
yshiraishiy <yuya0505soccer@gmail.com>
2025-01-30 15:37:17 +0900

    ✨ノートを編集する機能の実装

--- src/App.jsx
+++ src/App.jsx
@@ -25,8 +25,20 @@ function App() {
   };
 
   const getActiveNote = () => {
-    return notes.find((note) => note.id === activeNote)
-  }
+    return notes.find((note) => note.id === activeNote);
+  };
+
+  const onUpdateNote = (updatedNote) => {
+    // 修正された新しいノートの配列を返す
+    const updatedNoteArray = notes.map((note) => {
+      if (note.id === updatedNote.id) {
+        return updatedNote;
+      } else {
+        return note;
+      }
+    });
+    setNotes(updatedNoteArray);
+  };
 
   return (
     <div className="App">
@@ -37,7 +49,7 @@ function App() {
         activeNote={activeNote}
         setActiveNote={setActiveNote}
       />
-      <Main activeNote={getActiveNote()} />
+      <Main activeNote={getActiveNote()} onUpdateNote={onUpdateNote} />
     </div>
   );
 }
