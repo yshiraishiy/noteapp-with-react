beaadb1c4e61d29e9f33578a5232a07ce94d1f8a
yshiraishiy <yuya0505soccer@gmail.com>
2025-01-30 15:09:21 +0900

    ✨選択したノートをプレビューに表示

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,12 @@ import React from "react";
 
 import "./Main.css";
 
-export const Main = () => {
+export const Main = ({ activeNote }) => {
+
+  if(!activeNote) {
+    return <div className="no-active-note">ノートが選択されていません</div>
+  }
+
   return (
     <div className="app-main">
       <div className="app-main-note-edit">
@@ -10,8 +15,8 @@ export const Main = () => {
         <textarea placeholder="ノート内容を記入"></textarea>
       </div>
       <div className="app-main-note-preview">
-        <h1 className="preview-title">タイトル</h1>
-        <div className="markdown-preview">ノート内容</div>
+        <h1 className="preview-title">{activeNote.title}</h1>
+        <div className="markdown-preview">{activeNote.content}</div>
       </div>
     </div>
   );
