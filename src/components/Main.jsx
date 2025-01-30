d12fa5b0a36e4acfc063c3324a813c92a82e69a7
yshiraishiy <yuya0505soccer@gmail.com>
2025-01-30 14:09:22 +0900

    ✨Mainコンポーネントの雛形を実装

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
-import "./Main.css"
+import "./Main.css";
 
 export const Main = () => {
   return (
-    <div>Main</div>
-  )
-}
+    <div className="app-main">
+      <div className="app-main-note-edit">
+        <input type="text" />
+        <textarea placeholder="ノート内容を記入"></textarea>
+      </div>
+      <div className="app-main-note-preview">
+        <h1 className="preview-title">タイトル</h1>
+        <div className="markdown-preview">ノート内容</div>
+      </div>
+    </div>
+  );
+};
