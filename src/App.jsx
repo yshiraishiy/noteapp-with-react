d98c102868371a1a2a322d270f4231b49a5bec20
yshiraishiy <yuya0505soccer@gmail.com>
2025-01-30 13:34:13 +0900

    ✨新しいノートを追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-import './App.css'
+import "./App.css";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-import { Sidebar } from './components/Sidebar'
-import { Main } from './components/Main'
+import { Sidebar } from "./components/Sidebar";
+import { Main } from "./components/Main";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  const onAddNote = () => {
+    const newNote = {
+      id: uuidv4(),
+      title: "新しいノート",
+      content: "新しいノートの内容",
+      modDate: Date.now(),
+    };
+    setNotes([...notes, newNote]);
+  };
 
   return (
-    <div className='App'>
-      <Sidebar />
+    <div className="App">
+      <Sidebar onAddNote={onAddNote} notes={notes} />
       <Main />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
