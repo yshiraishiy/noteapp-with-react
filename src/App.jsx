ed46f4774ce64f628888aff1033bec07c381d7b7
yshiraishiy <yuya0505soccer@gmail.com>
2025-01-30 13:10:40 +0900

    ✨サイドバーの雛形を実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 
+import { Sidebar } from './components/Sidebar'
+import { Main } from './components/Main'
+
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className='App'>
+      <Sidebar />
+      <Main />
+    </div>
   )
 }
 
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import "./Sidebar.css";
+
+export const Sidebar = () => {
+  return (
+    <div className="app-sidebar">
+      <div className="app-sidebar-header">
+        <h1>ノート</h1>
+        <button>追加</button>
+      </div>
+      <div className="app-sidebar-notes">
+        <div className="app-sidebar-note">
+          <div className="sidebar-note-title">
+            <strong>タイトル</strong>
+            <button>削除</button>
+          </div>
+          <p>ノートの内容です</p>
+          <small>最後の修正日:xx/xx</small>
+        </div>
+      </div>
+    </div>
+  );
+};
